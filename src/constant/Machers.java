b6f57abfda02b0013c61d036e035c04f7195e1e1
Your Name <you@example.com>
2017-12-03 17:13:23 +0800

    ss

---
+++ src/constant/Machers.java
@@ -0,0 +1,14 @@
+package constant;
+
+import java.util.regex.Pattern;
+
+public class Machers {
+	public static Pattern numP = Pattern.compile("[+,-]?[0-9]+\\.?[0-9]*");
+	public static Pattern idenPattern = Pattern
+			.compile("([a-z,A-Z])|(([a-z,A-Z])"
+					+ "([a-z,A-Z]|[0-9]|_)*([a-z,A-Z]|[0-9]))"); // 标识符的正则表达式
+	public static Pattern idenArrPattern = Pattern
+			.compile("(([a-z,A-Z])|(([a-z,A-Z])"
+					+ "([a-z,A-Z]|[0-9]|_)*([a-z,A-Z]|[0-9])))"
+					+ "\\[[0-9]+\\]"); // 标识符数组的正则表达式
+}
